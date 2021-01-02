5ed50f84838298a96a55c886ecacd14c3a1f3eb0
Joke <joke.bergmans@student.hogent.be>
2021-01-02 12:26:38 +0100

    Format timespan

---
+++ TravelList/TravelList/Utils/TimeSpanStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace TravelList.Utils
+{
+    public class TimeSpanStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((DateTime)value).ToString("HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return DateTime.Parse((string)value);
+        }
+    }
+}
